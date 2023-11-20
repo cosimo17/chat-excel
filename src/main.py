3a57464fbfdb608babda355b30943756b1f13bb6
JiaQi <526174835@qq.com>
2023-11-20 08:26:34 +0800

    remove useless code

--- src/main.py
+++ src/main.py
@@ -199,23 +199,6 @@ class Main(QWidget):
         self.load_data_with_pandas(filename)
         self.loaded = True
 
-    def load_data(self, excel_file):
-        workbook = openpyxl.load_workbook(excel_file)
-        sheet = workbook.active
-
-        self.table_widget.setRowCount(sheet.max_row)
-        self.table_widget.setColumnCount(sheet.max_column)
-
-        list_values = list(sheet.values)
-        self.table_widget.setHorizontalHeaderLabels(list_values[0])
-
-        row_index = 0
-        for value_tuple in list_values[1:]:
-            col_index = 0
-            for value in value_tuple:
-                self.table_widget.setItem(row_index, col_index, QTableWidgetItem(str(value)))
-                col_index += 1
-            row_index += 1
 
     def load_data_with_pandas(self, excel_file):
         df = pd.read_excel(excel_file)
